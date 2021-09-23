929ab14cc415d5ac9dd1df279962bf5415752528
Vincent Salamand <salamandvincent@gmail.com>
2021-09-23 12:52:34 +0200

    use Hugging Face + add HF api key in env + uninstall spacy

--- classifier.py
+++ classifier.py
@@ -1,15 +1,37 @@
-from pathlib import Path
-import spacy
-import html
+import requests
+import pandas as pd
+from dotenv import load_dotenv
+import os
 
+load_dotenv()  # take environment variables from .env.
 
-model_dir = Path('models/spacy-recipe-classifier')
-nlp = spacy.load(model_dir)
+HF_API_KEY = os.environ.get("HF_API_KEY")
+
+API_URL = "https://api-inference.huggingface.co/models/vsalamand/autonlp-fr-ingredient-classifier-11272321"
+headers = {"Authorization": HF_API_KEY}
+
+def query(payload):
+  response = requests.post(API_URL, headers=headers, json=payload)
+  return response.json()
 
 def get_ingredients(text_list):
-    docs = list(nlp.pipe(text_list))
-    predictions = []
-    for doc in docs:
-        if max(doc.cats, key=doc.cats.get) == "ingredient":
-            predictions.append(html.unescape(doc.text))
-    return predictions
+  try:
+    # cut strings max characters to limit HF API usage based on characters
+    inputs = [string[:70] for string in text_list]
+
+    # output is a list of list of dic with label and score
+    output = query(inputs)
+
+    # extract label predicted for each input
+    preds = []
+    for result in output:
+      preds.append(max(result, key=lambda d: d['score'])['label'])
+
+    # return list of text inputs that correspond to ingredients
+    df = pd.DataFrame(list(zip(text_list, preds)), columns = ['text', 'preds'])
+    df_ingredients = df[df["preds"] == '1']
+    return df_ingredients.text.to_list()
+
+  except:
+    return None
+
